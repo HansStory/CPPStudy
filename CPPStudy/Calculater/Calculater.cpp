6ec8e51b3921f75ef78e943a0fce5bcb053ba718
Han Stroy <gkswjdgur552@gmail.com>
2024-12-26 15:44:08 +0900

    Fix Calculater.cpp ( 1번 과제 필수 기능 - 합과 평균 구하기 )

--- CPPStudy/Calculater/Calculater.cpp
+++ CPPStudy/Calculater/Calculater.cpp
@@ -2,8 +2,46 @@
 
 using namespace std;
 
+void SetInputValue(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << i + 1 << "번째 수를 입력하세요. : ";
+		cin >> arr[i];
+	}
+}
+
+int const Sum(int arr[], int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	
+	return sum;
+}
+
+double const Average(int sumValue, int size, bool b_showDecimal = true)
+{
+	double &&avg = (double)sumValue / (double)size;
+	return b_showDecimal ? avg : (int)avg;
+}
+
 int main()
 {
+	int size = 5;
+	int* arr = new int[size];
+
+	// 입력값 세팅
+	SetInputValue(arr, size);
+
+	// 합계
+	int&& sumValue = Sum(arr, size);
+	cout << "합계는 : " << sumValue << " 입니다!" << endl;
+
+	// 평균
+	cout << "평균은 : " << Average(sumValue, size) << " 입니다!";
 
 	return 0;
 }
